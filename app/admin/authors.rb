99a5856ea85d2a8451a3e6974bb9745e0ecdf6d4
Shinichi Masuda <masuda@my-style.jp>
2019-08-04 19:23:38 +0900

    画面修正

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -1,17 +1,27 @@
 ActiveAdmin.register Author do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
   permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column '書籍数' do |author|
+      author.books.count
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do |author|
+    attributes_table(*author.class.columns.collect { |column| column.name.to_sym })
+    panel "書籍一覧" do
+      table_for author.books do
+        column :title
+        column :publish_date
+      end
+    end
+    active_admin_comments
+  end
   
 end
--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,17 +1,13 @@
 ActiveAdmin.register Book do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
   permit_params :author_id, :title, :publish_date
+
+  form do |f|
+    f.inputs do
+      f.input :author
+      f.input :title
+      f.input :publish_date, as: :datepicker
+    end
+    f.actions
+  end
   
 end
