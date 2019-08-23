3174deab0d2160565fe1a88dd83d6ad261d90162
Shinichi Masuda <masuda@systemplanet.co.jp>
2019-08-23 15:19:29 +0900

    User調整

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register User do
-    permit_params :name, :nickname, :image, :email    
+    permit_params :name, :nickname, :image, :email 
+    
+    index do
+      selectable_column
+      id_column
+      column :email
+      column :confirmed_at
+      column :current_sign_in_at
+      column :sign_in_count
+      column :created_at
+      actions
+    end
   end
