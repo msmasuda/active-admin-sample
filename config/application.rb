1ea8dfdeeb142c2d2f09669c64206f96c3176cc4
Shinichi Masuda <masuda@systemplanet.co.jp>
2019-08-30 15:15:20 +0900

    rack-core設定追加

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,18 @@ module AdminSample
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+  
+    # rack-cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        # 許可するドメイン
+        origins "*"
+        # 許可するヘッダとメソッドの種類
+        resource "*",
+                 headers: :any,
+                 methods: [:get, :post, :patch, :delete, :head, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
